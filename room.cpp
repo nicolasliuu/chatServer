c8637bd4308a64018977059bf34e4df37a736749
Nicolas Liu <nicolasliu18@gmail.com>
2023-12-10 18:58:12 -0500

    cooking Room and read instructions 20x

--- room.cpp
+++ room.cpp
@@ -7,20 +7,28 @@
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
   // TODO: initialize the mutex
+  pthread_mutex_init(&lock, NULL);
 }
 
 Room::~Room() {
   // TODO: destroy the mutex
+  pthread_mutex_destroy(&lock);
 }
 
 void Room::add_member(User *user) {
+  Guard guard(lock);
   members.insert(user);
 }
 
 void Room::remove_member(User *user) {
+  Guard guard(lock);
   members.erase(user);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // TODO: send a message to every (receiver) User in the room
+  Guard guard(lock);
+  for (User *user : members) {
+    user->mqueue.push(Message(TAG_DELIVERY, room_name + ":" + sender_username + ":" + message_text));
+  }
 }
